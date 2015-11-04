f57b0743b62d2dcc28e98006456fe8ca5906de59
Dmitriy Pilipenko <dmitriy.pilipenko@gmail.com>
2015-11-03 21:02:11 -0500

    insert and select from db

--- index.js
+++ index.js
@@ -1,10 +1,77 @@
+// Keys
+var connString = "postgres://mixtape@localhost/mixtape";
+
+var pg = require('pg');
+var Promise = require('bluebird');
 var express = require('express');
 var app = express();
+
+
+// Helpers
+function getVisitCount(callback) {
+  pg.connect(connString, function(err, client, done) {
+    if (err) {
+      console.error('error fetching client from pool', err);
+      return callback(-1);
+    }
+    var query = "SELECT COUNT(*) FROM visits";
+    client.query(query, function (err, result) {
+      done();
+      if (err) {
+        console.error('error running query', err);
+        return callback(-1);
+      }
+      var count = result.rows[0].count;
+      console.info('fetched count of all visits: ' + count);
+      return callback(count);
+    });
+  });
+}
+
+function addVisit(callback) {
+  pg.connect(connString, function(err, client, done) {
+    if (err) {
+      console.error('error fetching client from pool', err);
+      callback();
+    }
+    var id = Math.floor((Math.random() * 100000) + 3);
+    var query = "INSERT INTO visits (ID) VALUES ('" + id + "');";
+    client.query(query, function (err, result) {
+      done();
+      if (err) {
+       console.error('error running query', err);
+       callback();
+      }
+      console.log('added one visit');
+      callback();
+    });
+  });
+}
+
+function createVisitsTable() {
+ pg.connect(connString, function(err, client, done) {
+   if (err) {
+     return console.error('error fetching client from pool', err);
+   }
+   var query = "CREATE TABLE visits(ID INT PRIMARY KEY NOT NULL);";
+   client.query(query, function (err, result) {
+     done();
+     if (err) {
+       return console.error('error running query', err);
+     }
+     console.log('created table visits');
+   });
+ });
+}
 
 // Routes
 // respond with "Hello World!" on the homepage
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  addVisit(function (result) {
+    getVisitCount(function (result) {
+      res.send('Hello World! Visit: ' + result);
+    });
+  });
 });
 
 // accept POST request on the homepage
